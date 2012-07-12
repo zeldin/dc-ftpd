a6fe26d283aa4b3cd8cbb1fc184fc5ca1d69ded5
Marcus Comstedt <marcus@mc.pp.se>
2012-07-12 15:12:48 +0200

    Whitespace fix

--- src/ftpd.c
+++ src/ftpd.c
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2002 Florian Schulze.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
@@ -14,7 +14,7 @@
  * 3. Neither the name of the authors nor the names of the contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -63,13 +63,13 @@ int dbg_printf(const char *fmt, ...);
 
 #define msg110 "110 MARK %s = %s."
 /*
-         110 Restart marker reply.
-             In this case, the text is exact and not left to the
-             particular implementation; it must read:
-                  MARK yyyy = mmmm
-             Where yyyy is User-process data stream marker, and mmmm
-             server's equivalent marker (note the spaces between markers
-             and "=").
+	 110 Restart marker reply.
+	     In this case, the text is exact and not left to the
+	     particular implementation; it must read:
+		  MARK yyyy = mmmm
+	     Where yyyy is User-process data stream marker, and mmmm
+	     server's equivalent marker (note the spaces between markers
+	     and "=").
 */
 #define msg120 "120 Service ready in nnn minutes."
 #define msg125 "125 Data connection already open; transfer starting."
@@ -83,65 +83,65 @@ int dbg_printf(const char *fmt, ...);
 #define msg213 "213 File status."
 #define msg214 "214 %s."
 /*
-             214 Help message.
-             On how to use the server or the meaning of a particular
-             non-standard command.  This reply is useful only to the
-             human user.
+	     214 Help message.
+	     On how to use the server or the meaning of a particular
+	     non-standard command.  This reply is useful only to the
+	     human user.
 */
 #define msg214SYST "214 %s system type."
 /*
-         215 NAME system type.
-             Where NAME is an official system name from the list in the
-             Assigned Numbers document.
+	 215 NAME system type.
+	     Where NAME is an official system name from the list in the
+	     Assigned Numbers document.
 */
 #define msg220 "220 lwIP FTP Server ready."
 /*
-         220 Service ready for new user.
+	 220 Service ready for new user.
 */
 #define msg221 "221 Goodbye."
 /*
-         221 Service closing control connection.
-             Logged out if appropriate.
+	 221 Service closing control connection.
+	     Logged out if appropriate.
 */
 #define msg225 "225 Data connection open; no transfer in progress."
 #define msg226 "226 Closing data connection."
 /*
-             Requested file action successful (for example, file
-             transfer or file abort).
+	     Requested file action successful (for example, file
+	     transfer or file abort).
 */
 #define msg227 "227 Entering Passive Mode (%i,%i,%i,%i,%i,%i)."
 /*
-         227 Entering Passive Mode (h1,h2,h3,h4,p1,p2).
+	 227 Entering Passive Mode (h1,h2,h3,h4,p1,p2).
 */
 #define msg230 "230 User logged in, proceed."
 #define msg250 "250 Requested file action okay, completed."
 #define msg257PWD "257 \"%s\" is current directory."
 #define msg257 "257 \"%s\" created."
 /*
-         257 "PATHNAME" created.
+	 257 "PATHNAME" created.
 */
 #define msg331 "331 User name okay, need password."
 #define msg332 "332 Need account for login."
 #define msg350 "350 Requested file action pending further information."
 #define msg421 "421 Service not available, closing control connection."
 /*
-             This may be a reply to any command if the service knows it
-             must shut down.
+	     This may be a reply to any command if the service knows it
+	     must shut down.
 */
 #define msg425 "425 Can't open data connection."
 #define msg426 "426 Connection closed; transfer aborted."
 #define msg450 "450 Requested file action not taken."
 /*
-             File unavailable (e.g., file busy).
+	     File unavailable (e.g., file busy).
 */
 #define msg451 "451 Requested action aborted: local error in processing."
 #define msg452 "452 Requested action not taken."
 /*
-             Insufficient storage space in system.
+	     Insufficient storage space in system.
 */
 #define msg500 "500 Syntax error, command unrecognized."
 /*
-             This may include errors such as command line too long.
+	     This may include errors such as command line too long.
 */
 #define msg501 "501 Syntax error in parameters or arguments."
 #define msg502 "502 Command not implemented."
@@ -151,17 +151,17 @@ int dbg_printf(const char *fmt, ...);
 #define msg532 "532 Need account for storing files."
 #define msg550 "550 Requested action not taken."
 /*
-             File unavailable (e.g., file not found, no access).
+	     File unavailable (e.g., file not found, no access).
 */
 #define msg551 "551 Requested action aborted: page type unknown."
 #define msg552 "552 Requested file action aborted."
 /*
-             Exceeded storage allocation (for current directory or
-             dataset).
+	     Exceeded storage allocation (for current directory or
+	     dataset).
 */
 #define msg553 "553 Requested action not taken."
 /*
-             File name not allowed.
+	     File name not allowed.
 */
 
 enum ftpd_state_e {
@@ -932,7 +932,7 @@ static void cmd_pasv(const char *arg, struct tcp_pcb *pcb, struct ftpd_msgstate 
 
 		if(++port > 0x7fff)
 			port = 4096;
-	
+
 		fsm->dataport = port;
 		err = tcp_bind(fsm->datapcb, &pcb->local_ip, fsm->dataport);
 		if (err == ERR_OK)
